f51de07ec30b9b27497fb7e8bf8df929f864f68c
Pavel Shakhov <ab.pongo@gmail.com>
2024-10-02 16:52:53 +0300

    Создал версию requestRapidApi используя fetch вместо axios

--- src/utils/rapidapi.js
+++ src/utils/rapidapi.js
@@ -45,4 +45,62 @@ async function requestRapidApi(method, url, { host, params, parseLimitsFn = unde
   }
 }
 
-module.exports = { requestRapidApi };
+async function requestRapidApiFetch(
+  method,
+  url,
+  { host, params, body = undefined, parseLimitsFn = undefined, parseJSON = true },
+) {
+  const parseLimits = parseLimitsFn ?? _parseLimits;
+
+  const options = {
+    method,
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": host,
+      "Content-Type": "application/json",
+    },
+    body: body ? JSON.stringify(body) : undefined,
+  };
+
+  if (params) {
+    const queryParams = new URLSearchParams(params).toString();
+    const sign = url.includes("?") ? "&" : "?";
+    url += `${sign}${queryParams}`;
+  }
+
+  try {
+    const response = await fetch(url, options);
+    const data = await (parseJSON ? response.json() : response.text());
+
+    if (!response.ok) {
+      throw { response, error: data };
+    }
+
+    return Ok({
+      ...parseLimits(response),
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+
+    if (error.response?.status === 429) {
+      // балансировщик прекращает делать запросы если remaining < 3
+      // поэтому 429 ошибки не должно возникать.
+      //
+      // если же она возникла, то это или лимит запросов в минуту/час, или изменения тарифов апи.
+      // в общем, это что-то необычное. поэтому reset делаем 1 час
+      return Err(error.message, { remaining: 0, reset: HOUR });
+    }
+
+    return Err(error, parseLimits(error.response));
+  }
+
+  function _parseLimits(response) {
+    return {
+      remaining: parseInt(response.headers.get("x-ratelimit-requests-remaining"), 10),
+      reset: parseInt(response.headers.get("x-ratelimit-requests-reset"), 10),
+    };
+  }
+}
+
+module.exports = { requestRapidApi, requestRapidApiFetch };
---
+++ test_integration/rapidapi.js
@@ -0,0 +1,51 @@
+"use strict";
+
+require("dotenv").config();
+const assert = require("node:assert/strict");
+const { describe, it, test } = require("node:test");
+const { requestRapidApiFetch } = require("../src/utils/rapidapi");
+
+describe("requestRapidApi", () => {
+  it.skip("GET with params", async () => {
+    const res = await requestRapidApiFetch("GET", "https://imdb8.p.rapidapi.com/v2/search", {
+      host: "imdb8.p.rapidapi.com",
+      params: {
+        searchTerm: "tom cruise",
+        type: "NAME",
+        first: "20",
+        country: "US",
+        language: "en-US",
+      },
+    });
+    // console.log(res);
+    assert.ok(res.isOk, res.isErr && `${res.error.name}: ${res.error.message}`);
+    assert.ok(Number.isInteger(res.value.remaining));
+    assert.ok(Number.isInteger(res.value.reset));
+    assert.ok(res.value.data.data.mainSearch);
+  });
+
+  it.skip("POST", async () => {
+    const res = await requestRapidApiFetch(
+      "POST",
+      "https://onecompiler-apis.p.rapidapi.com/api/v1/run",
+      {
+        host: "onecompiler-apis.p.rapidapi.com'",
+        body: {
+          language: "python",
+          stdin: "Peter",
+          files: [
+            {
+              name: "index.py",
+              content: "import sys\nname = sys.stdin.readline()\nprint('Hello '+ name)",
+            },
+          ],
+        },
+      },
+    );
+    // console.log(res);
+    assert.ok(res.isOk, res.isErr && `${res.error.name}: ${res.error.message}`);
+    assert.ok(Number.isInteger(res.value.remaining));
+    assert.ok(Number.isInteger(res.value.reset));
+    assert.deepEqual(res.value.data.status, "success");
+  });
+});
